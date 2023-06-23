626b5e52a945441ee1167e6763dfce9fd82a20be
tuyen10a6 <92588348+tuyen10a6@users.noreply.github.com>
2023-06-24 00:28:43 +0700

    Render Image Product HomePage

--- Project/Areas/Customer/Controllers/HomeController.cs
+++ Project/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Project.Models;
+using ProjectBook.DataAccess.Repository.IRepository;
 using System.Diagnostics;
 
 namespace ProjectBookWeb.Areas.Customer.Controllers
@@ -9,15 +10,19 @@ namespace ProjectBookWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
+
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
